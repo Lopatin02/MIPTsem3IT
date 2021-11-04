0200f199d65d384b838b15920a7dbb55f5bfdab5
Lopatin02 <71967029+Lopatin02@users.noreply.github.com>
2021-11-04 15:39:58 +0300

    Create 09_stvfs.c

---
+++ 09_stvfs.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <sys/param.h>
+#include <sys/mount.h>
+#include <sys/statvfs.h>
+
+int main(int argc, char *argv[])
+{
+    if (argc != 2)
+    {
+        fprintf(stderr, "Usage: %s <pathname> \n", argv[0]);
+        return 1;
+    }
+    struct statvfs buf;
+    
+	if (statvfs(argv[1], &buf) == -1)
+    {
+        perror("Failed to statfs");
+        return 1;
+    }
+
+    printf("Total: %lu\n", buf.f_frsize*buf.f_blocks);
+    printf("Available: %lu\n", buf.f_frsize*buf.f_bavail);
+    printf("Used: %lu\n", buf.f_frsize*buf.f_blocks - buf.f_frsize*buf.f_bfree);
+    return 0;
+}
