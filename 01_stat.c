b7676906e93fa8ef5b6f390b215409f40ee1e222
VladimirLopatin <lopatin.va@phystech.edu>
2021-12-17 23:00:25 +0300

    Fix program 01_stat

--- 01_stat.c
+++ 01_stat.c
@@ -1,71 +1,104 @@
-#define _FILE_OFFSET_BITS 64
-
-
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <time.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/sysmacros.h>
-#include <errno.h>
+#include <string.h>
+#include <linux/stat.h>
+#include <linux/fcntl.h>
+#include <sys/time.h>
+#include <unistd.h>
+#include <stdint.h>
+#include <grp.h>
+#include <pwd.h>
 
+//выводит время в милисекундах, timespec - есть наносекунды
+static void print_time(const char *type, const struct timespec *ts) {
 
+        time_t tv_sec = (time_t) ts->tv_sec;
+        char buffer[sizeof("YYYY-mm-dd HH:MM:SS")];//оперделяем buffer длинной в 20 символов, используя ф-ю sizeof. YYYY-mm-dd HH:MM:SS - это вид вывода.
+        struct tm *tm = localtime(&tv_sec);
+        strftime(buffer, sizeof(buffer), "%Y-%m-%d %H:%M:%S", tm);//в буффер кладем время из tm
+        printf ("%s %s.%09lu ", type, buffer, ts->tv_nsec);//%s %s.%09lu - 09lu - вывод 9 символов с нуля unsigned long
+        strftime(buffer, sizeof(buffer), "%z", tm);
+        printf("%s\n", buffer);
+}
 
-const char* device_type(const mode_t mode) {
-    switch (mode & S_IFMT) {
-        case S_IFBLK:   return "block device";
-        case S_IFCHR:   return "character device";
-        case S_IFDIR:   return "directory";
-        case S_IFIFO:   return "FIFO/pipe";
-        case S_IFLNK:   return "symbolic link";
-        case S_IFREG:   return "regular file";
-        case S_IFSOCK:  return "socket";
-    }
-    return "unknown";
+//определяем тип устр-ва, которое получаем
+const char * filetype (unsigned int mode)
+{
+	switch (mode & S_IFMT) {
+                case S_IFBLK: return("block device");
+                case S_IFCHR: return("character device");
+                case S_IFDIR: return("directory");
+                case S_IFIFO: return("FIFO/pipe");
+                case S_IFLNK: return("symlink");
+                case S_IFREG: return("regular file");
+                case S_IFSOCK: return("socket");
+                default: return("unknown?");
+	}
+}
+
+//Права доступа к файлу. Права: права пользователя, права группы и остальные права 
+char * rights (mode_t mode)
+{
+	char * mode_rights = malloc (sizeof("rwxrwxrwx"));//права записываются в виде строки из 3-х троек rwx
+	int i = 8;
+	for (i = 8; i >= 0; i--)
+	       mode_rights[8-i] = (mode & (1 << i) ? "xwr"[i%3]: '-');
+
+	if (S_ISUID & mode) //S_ISUID - бит set-user-ID
+		mode_rights[2] = 's';
+	if (S_ISGID & mode) //бит set-group-ID
+		mode_rights[5] = 's';
+	if (S_ISVTX & mode)//sticky бит
+		mode_rights[8] = 't';
+	mode_rights[9] = '\0';
+	return mode_rights;
+}
+
+// Имя пользователя
+const char * user_name(uid_t uid) {
+	struct passwd *info = getpwuid(uid);//возвращает указатель на структуру, содержащую поля строки из файла /etc/passwd для записи, совпадающей с идентификатором пользователя uid
+	return (info == NULL) ? NULL : info -> pw_name;
+}
+//Имя группы
+const char * group_name(uid_t uid) { 
+	struct group *info = getgrgid(uid);//возвращает указатель на структуру, содержащую информацию из файла /etc/group о группе, идентификатор которой совпадает с gid
+	return (info == NULL) ? NULL : info -> gr_name;
 }
 
 
 int main(int argc, char* argv[]) {
-    if(argc != 2) {
+    if(argc != 2) {//проверка на то, что подал неправильное кол-во файлов
         fprintf(stderr, "Usage: %s <pathname>\n", argv[0]);
         exit(EXIT_FAILURE);
     }
 
     struct stat sb;
 
-    if(lstat(argv[1], &sb) == -1) { //lstat = lstat , но однако lstat - о ссылке, stat - про файл
+    if(lstat(argv[1], &sb) == -1) { //проверка, что lsat не вывел ошибку  stat = lstat , но однако lstat - о ссылке, stat - про файл
         perror("lstat");
         exit(EXIT_FAILURE);
     }
 
-    printf("File type:              %s\n", device_type(sb.st_mode));
-    printf("INode:                  %ld\n", (long)sb.st_ino);
-    printf("Access mode:            %lo (octal)\n", (unsigned long)sb.st_mode);
-    printf("number of links:        %ld\n", (long)sb.st_nlink);
-    printf("Owner:                  UID=%ld\tGID=%ld\n", (long)sb.st_uid, (long)sb.st_gid);
-    printf("preferred block size:   %ld byte\n", (long)sb.st_blksize);
-    printf("File size:              %lld byte\n", (long long)sb.st_size);
-    printf("Allocated blocks:       %lld\n", (long long)sb.st_blocks);
+    printf("File name %s\n", argv[1]);
+    printf("ID of containing device: [%lxh, %ldd]\n", (long) major(sb.st_dev), (long) minor(sb.st_dev));
+    printf("File type: %s\n", filetype(sb.st_mode));//передаем в функцию параметры и получаем тип файла
+    printf("I-node number: %ju\n", (uintmax_t) sb.st_ino);
+    char *permissions_str = rights(sb.st_mode);//получаем из фунции права
+    printf("Mode_rights: (%04o/%s)\n", sb.st_mode & ALLPERMS, permissions_str);
+    free(permissions_str);
+    printf("Link count: %ld\n", (long) sb.st_nlink);
+    printf("Ownership UID=(%ld/%s)   GID=(%ld/%s)\n", (long) sb.st_uid, user_name(sb.st_uid), (long) sb.st_gid, group_name(sb.st_gid));
+    printf("Preferred I/O block size: %ld bytes\n", (long) sb.st_blksize);
+    printf("File size: %lld bytes\n", (long long) sb.st_size);
+    printf("Blocks allocated: %lld\n", (long long) sb.st_blocks);
 
-    const size_t time_str_size = sizeof("Day Mon dd hh:mm:ss yyyy\n");
-
-    struct tm curr_time;
-    char buf[time_str_size];
-
-    const char fmt[] = "%a %b %d %H:%M:%S %Y";
-    
-    if(!strftime(buf, sizeof(buf), fmt, gmtime_r(&sb.st_ctime, &curr_time))) {
-        exit(EXIT_FAILURE);
-    }
-    printf("C_TIME                  %s\n", buf);
-    if(!strftime(buf, sizeof(buf), fmt, gmtime_r(&sb.st_atime, &curr_time))) {
-        exit(EXIT_FAILURE);
-    }
-    printf("A_TIME                  %s\n", buf);
-    if(!strftime(buf, sizeof(buf), fmt, gmtime_r(&sb.st_mtime, &curr_time))) {
-        exit(EXIT_FAILURE);
-    }
-    printf("M_TIME                  %s\n", buf);
-
+    print_time("Last status change:	  ", &sb.st_ctim);
+    print_time("Last file access:         ", &sb.st_atim);
+    print_time("Last file modification:   ", &sb.st_mtim);
+   
     return 0;
 }
