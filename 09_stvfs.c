7c58280ca001e67c2783494185f82284ce035658
VladimirLopatin <lopatin.va@phystech.edu>
2021-12-18 23:40:52 +0300

    add comments, change a bit 09_stvfs.c

--- 09_stvfs.c
+++ 09_stvfs.c
@@ -1,6 +1,4 @@
-//вывести занятое, свободное и доступное (для использования) пространство для 
-//файловой системы, в которой лежит указанный файл или каталог
-//statfs statvfs кого использовать, а кто linux specific
+//нам нужно в этой задаче вывести свободное и доступное(для использования) пространство для файловой системы, в которой будет лежать данный файл или каталог
 
 #include <stdio.h>
 #include <sys/param.h>
@@ -9,21 +7,23 @@
 
 int main(int argc, char *argv[])
 {
-    if (argc != 2)
+    if (argc != 2)//ввод неверного числа параметров
     {
-        fprintf(stderr, "Usage: %s <pathname> \n", argv[0]);
-        return 1;
-    }
-    struct statvfs buf;
-    
-	if (statvfs(argv[1], &buf) == -1)
-    {
-        perror("Failed to statfs");
+        printf("Usage: %s <pathname> \n", argv[0]);
         return 1;
     }
 
-    printf("Total: %lu\n", buf.f_frsize*buf.f_blocks);
-    printf("Available: %lu\n", buf.f_frsize*buf.f_bavail);
-    printf("Used: %lu\n", buf.f_frsize*buf.f_blocks - buf.f_frsize*buf.f_bfree);
+    struct statvfs buf;
+   //функция statvfs возвращает информацию о смонтированной файловой системе
+    
+	if (statvfs(argv[1], &buf) == -1)//В случае успеха возвращается ноль. В случае ошибки возвращается -1
+    {
+        perror("Failed to statfs");//ошибка работы функции statvfs
+        return 1;
+    }
+
+    printf("Total: %lu\n", buf.f_frsize * buf.f_blocks);//Fragment size - f_frsize, также f_blocks;   /* Size of fs - fragment size in f_frsize units */  
+    printf("Available: %lu\n", buf.f_frsize * buf.f_bavail);//Number of free blocks for unprivileged users
+    printf("Used: %lu\n", buf.f_frsize * buf.f_blocks - buf.f_frsize * buf.f_bfree);//f_bfree - Number of free blocks
     return 0;
 }
