fdf622ed7c9708223fcef5a0b671b51bb30ca62d
VladimirLopatin <lopatin.va@phystech.edu>
2021-12-18 23:02:46 +0300

    Fix program, add sigaction

--- 15_1_signal.c
+++ 15_1_signal.c
@@ -1,30 +1,38 @@
+//kill -9  3114 - завершает программу
+//надо написать программу, которая будет сопротивляться своему завершению
 #include <unistd.h>
 #include <signal.h>
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-//нельзя так
-#define N 65
 
-volatile int g_last_signal;
+volatile int g_last_signal;//информирует компилятор, что значение переменной может меняться извне. Это может произойти под управлением операционной системы, аппаратных средств или другого потока. Поскольку значение может измениться, компилятор каждый раз загружает его из памяти
 
-void proc_info(void) {
+void proc_info(void) 
+{//информацию о процессе выдаем
     printf("PID %d, PPID %d, PGID %d, SID %d, UID %d, GID %d\n", getpid(), getppid(), getpgrp(), getsid(0), getuid(), getgid());
 }
 
-void sig_handler(int signum) {
-    g_last_signal = signum;
+void sig_handler(int s_number) 
+{
+    g_last_signal = s_number;
 }
 
-int main(void) {
+int main(void) 
+{
 
     proc_info();
     
-    for(int i = 0; i < N; i++) {
-        signal(i, sig_handler);//sigaction, signal старый
+    for(int i = 0; i < NSIG; i++) 
+    {
+	struct sigaction s_act;//Системный вызов sigaction используется для изменения действий процесса при получении соответствующего сигнала.
+        memset(&s_act, 0, sizeof(s_act));
+        s_act.sa_handler = sig_handler;
+        sigaction(i, &s_act, NULL);
     }
 
-    while(1) {
+    while(1) 
+    {
         pause();
         printf("last_signal = %d (%s)\n", g_last_signal, strsignal(g_last_signal));
     }
