b82f5559de09e740dcce30f12140c8827cd4daa4
VladimirLopatin <lopatin.va@phystech.edu>
2021-12-18 13:58:48 +0300

    Fix program 3_copy

--- 3_copy.c
+++ 3_copy.c
@@ -1,14 +1,16 @@
+//Цель задачи: скопировать данные из одного файла в другой
 #include <stdlib.h>
+#include <stdio.h>
 #include <unistd.h>
 #include <fcntl.h>
-#include <errno.h>
-#include <stdio.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <stdint.h>
+#define BUF_SIZE 1048576
 
-
-#define DEFAULT_CHUNK   262144
-
-
-ssize_t write_all(int fd, const void *buf, size_t count) { // signed size_t
+ssize_t write_all(int fd, const void *buf, size_t count) {//создаем эту функцию, так как у write partial success и не се может записаться
     /* давайте мы будет вызывать write, пока не возникнет ошибка, либо пока все не запишем*/
     size_t bytes_written = 0;
     while(bytes_written < count) {
@@ -18,49 +20,57 @@ ssize_t write_all(int fd, const void *buf, size_t count) { // signed size_t
         }
         bytes_written += res;
     }
+
     return (ssize_t)bytes_written;
 }
 
 
-int copy_file(int src_fd, int dest_fd) {
+int copy_file(int src_fd, int dest_fd) {//src_fd - откуда копируем, dest_fd - куда копировать
     ssize_t bytes = 0;
-    int err;
-    char* data = (char*)malloc(DEFAULT_CHUNK);
-    if(!data) {
+    char* data = (char*)malloc(BUF_SIZE);//data - выдел память(читаем блоками)
+    if(!data) {//malloc не ошибся
         close(src_fd);
         close(dest_fd);
-        return ENOMEM;
+	
+	perror("Failed memory allocation");
+        return 6;
     }
+
     while(1) {
         bytes = read(src_fd, data, sizeof(data));
-        if(bytes == -1) {
-            err = errno;
-            close(src_fd);
-            close(dest_fd);
-            return err;
-        }
-        if(bytes == 0) {
-            break;
-        }
-        bytes = write_all(dest_fd, data, bytes);
-        if(bytes < 0) {
-            err = EIO;
-            if(bytes == -1) {
-                err = errno;
-            }
+
+        if(bytes == -1) {//проверка, что read не ошибся
             free(data);
             close(src_fd);
             close(dest_fd);
-            return err;
+	                
+	    perror("File reading error");
+            return 7;
+        }
+
+        if(bytes == 0) {// выход из цикла
+            break;
+        }
+
+        bytes = write_all(dest_fd, data, bytes);//записываем в write_all, bytes(число байтов), data - указатель на начало блока, в который считали
+
+        if(bytes < 0) {//если write ошибся
+            free(data);
+            close(src_fd);
+            close(dest_fd);
+
+            perror("File writing error");  
+            return 8;
         }
     }
+
     free(data);
     return 0;
 }
 
 
 int main(int argc, char* argv[]) {
-    if(argc != 3) {
+    if(argc != 3) {//проверка, что передали верное кол-во аргументов
         fprintf(stderr, "Usage: %s path text\n", argv[0]);
         return 1;
     }
@@ -81,11 +91,11 @@ int main(int argc, char* argv[]) {
         fprintf(stderr, "Failed to copy to %s from %s", argv[1], argv[2]);
         return 4;
     }
-    if (close(src_fd)) {
+    if (close(in_fd)) {
         perror("File close error");
         return 5;
     }
-    if (close(dest_fd)) {
+    if (close(out_fd)) {
         perror("File close error");
         return 6;
     }
