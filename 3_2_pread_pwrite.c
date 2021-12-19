72b8dc25bab76ded8eabcd8d9a67467e78575d7a
VladimirLopatin <lopatin.va@phystech.edu>
2021-12-19 14:00:12 +0300

    fix, change function 3_2

--- 3_2_pread_pwrite.c
+++ 3_2_pread_pwrite.c
@@ -7,9 +7,10 @@
 #include <sys/stat.h>
 #include <sys/sysmacros.h>
 
-#define BUF_SIZE (1024*1024)ЖцйЖцй:wq
+#define BUF_SIZE (2)
 
-int main(int argc, char const *argv[]) {
+int main(int argc, char const *argv[]) 
+{
     int result = 0;
     
     if (argc != 3) {
@@ -23,8 +24,8 @@ int main(int argc, char const *argv[]) {
         return 2;
     }
 
-    if (((sb.st_mode) & (S_IFMT)) != S_IFREG) {
-        perror("Not a regular file");
+    if (!S_ISREG(sb.st_mode)) {
+        fprintf(stderr, "Not a regular file\n");
         return 3;
     }
 
@@ -37,29 +38,34 @@ int main(int argc, char const *argv[]) {
     int fd2 = open (argv[2], O_RDWR | O_CREAT | O_TRUNC, 0644);
     if (fd2 < 0) {
         perror("Failed to open or create dest file");
+        close(fd1);
         return 5;
     }
 
     char buf[BUF_SIZE];
-    ssize_t nbytes, nbytes_w;
+    ssize_t nbytes, nbytes_w, bytes_bufr;
 
     off_t offset_r = 0, offset_w = 0;
     while ((nbytes = pread(fd1, buf, BUF_SIZE, offset_r)) > 0) {
         offset_r += nbytes;
-        while (nbytes) {
-            nbytes_w = pwrite(fd2, buf, nbytes, offset_w);
+        bytes_bufr = 0;
+        while (bytes_bufr < nbytes) {
+            nbytes_w = pwrite(fd2, buf + bytes_bufr, (size_t)(nbytes - bytes_bufr), offset_w);
             if (nbytes_w == -1) {
                 perror("pwrite failed");
+                close(fd1);
+                close(fd2);
                 return 6;
             }
-            nbytes -= nbytes_w;
+             
+            bytes_bufr += nbytes_w;
             offset_w += nbytes_w;
         }
     }
 
     if (nbytes == -1) {
         perror("pread failed");
-        return 7;
+        result = 7;
     }
 
     if (close(fd1) < 0) {
